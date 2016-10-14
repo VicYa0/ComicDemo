c24a065838cd9a61f8f3b5bcc9cac59a76e61c2e
VicYa0 <vic_ya0@163.com>
2016-10-14 18:38:54 +0800

    数据实体类构建，model层搭建（未完成）

--- app/src/main/java/vic/comicdemo/MainActivity.java
+++ app/src/main/java/vic/comicdemo/MainActivity.java
@@ -2,50 +2,25 @@ package vic.comicdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.thinkland.sdk.android.DataCallBack;
-import com.thinkland.sdk.android.JuheData;
 import com.thinkland.sdk.android.Parameters;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import vic.comicdemo.utils.UrlFactory;
+import vic.comicdemo.model.ComicModel;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.test)
     TextView mTest;
-    private final String NULL_PARAM ="";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        ComicModel model = new ComicModel();
         Parameters params = new Parameters();
-//        params.add("name",null);
-//        params.add("type",null);
-//        params.add("skip",0);
-//        params.add("finish",null);
-        JuheData.executeWithAPI(this, 163, UrlFactory.getComicListUrl(), JuheData.GET, params, new DataCallBack() {
-            @Override
-            public void onSuccess(int i, String s) {
-                mTest.setText(s);
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-
-            @Override
-            public void onFailure(int i, String s, Throwable throwable) {
-
-            }
-        });
-        //mTest.setText();
+        model.getComicList(this,params);
     }
 }
--- app/src/main/java/vic/comicdemo/model/ComicModel.java
+++ app/src/main/java/vic/comicdemo/model/ComicModel.java
@@ -1,13 +1,62 @@
 package vic.comicdemo.model;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.thinkland.sdk.android.DataCallBack;
+import com.thinkland.sdk.android.JuheData;
+import com.thinkland.sdk.android.Parameters;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import vic.comicdemo.entity.ComicListResult;
+import vic.comicdemo.utils.UrlFactory;
 
 /**
  * Created by Vic Yao on 2016/10/14.
  */
 
 public class ComicModel {
-    public List<ComicListResult> mComicListResults;
+    private List<ComicListResult> BeanList;
+    private List<ComicListResult.BookListBean> mBookList;
+    private ComicListResult mComicListResult;
+    private static final int API_ID = 163;
+    public void getComicList(Context context,Parameters parameters){
+        Parameters params = parameters;
+        JuheData.executeWithAPI(context, API_ID, UrlFactory.getComicListUrl(), JuheData.GET, params, new DataCallBack() {
+            @Override
+            public void onSuccess(int i, String s) {
+                try {
+                    JSONObject json = new JSONObject(s);
+                    if (json.getInt("error_code")==200){
+                        String result = (json.getJSONObject("result")).toString();
+//                        Log.i("result11",result);
+                        mComicListResult = new Gson().fromJson(result,ComicListResult.class);
+                        BeanList = new ArrayList<ComicListResult>();
+                        mBookList = new ArrayList<ComicListResult.BookListBean>();
+                        BeanList.add(mComicListResult);
+                        mBookList = BeanList.get(0).getBookList();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onFailure(int i, String s, Throwable throwable) {
+
+            }
+        });
+    }
 }
